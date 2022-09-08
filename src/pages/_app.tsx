e36539cad2d76613553c9489eecddf1f97db57df
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2022-09-08 18:09:54 -0300

    Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,18 @@ import { globalStyles } from "../styles/global";
 
 globalStyles();
 
+import logoImg from "../assets/logo.svg";
+
+import { Container, Header } from "../styles/pages/app";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
